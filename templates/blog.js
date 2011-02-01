9f45f860b9d8afc4e8e2f73b4de0899a01dcad4b
Steven Haddox <steven@haddox.us>
2011-01-31 21:37:29 -0500

    template updates

--- templates/blog.js
+++ templates/blog.js
@@ -1,4 +1,5 @@
 // blog theme JavaScript
 $(document).ready(function(){
-
+  // $("#pagebody").attr('style','max-height: 2300px; overflow: auto;');
+  $("#pagebody").mCustomScrollbar("vertical",400,"easeOutCirc",1.05,"auto","yes","yes",10);
 });
