0c5635686ac406bcd367aaa19c381b47256875c5
Steven <steven@SLK-MBP.local>
2011-02-05 10:47:40 -0500

    nearly final theme implementation

--- templates/blog.js
+++ templates/blog.js
@@ -1,13 +1,14 @@
 // blog theme JavaScript
 $(document).ready(function(){
-  $("#pagebody").mCustomScrollbar("vertical",400,"easeOutCirc",1.05,"fixed","yes","yes",15);
+  // $("#pagebody").mCustomScrollbar("vertical",400,"easeOutCirc",1.05,"fixed","yes","yes",15);
 });
 
+
 /* function to fix the -10000 pixel limit of jquery.animate */
-$.fx.prototype.cur = function(){
-    if ( this.elem[this.prop] != null && (!this.elem.style || this.elem.style[this.prop] == null) ) {
-      return this.elem[ this.prop ];
-    }
-    var r = parseFloat( jQuery.css( this.elem, this.prop ) );
-    return typeof r == 'undefined' ? 0 : r;
-}
+// $.fx.prototype.cur = function(){
+//     if ( this.elem[this.prop] != null && (!this.elem.style || this.elem.style[this.prop] == null) ) {
+//       return this.elem[ this.prop ];
+//     }
+//     var r = parseFloat( jQuery.css( this.elem, this.prop ) );
+//     return typeof r == 'undefined' ? 0 : r;
+// }
